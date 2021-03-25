b0b793f3cc25ea0e1d4530d6fca6b3eac97938d8
Ben Koo <koo0905@gmail.com>
2021-03-25 09:29:16 +0800

    Added the backup and restore scripts

--- dockerizedWiki/LocalSettings.php
+++ dockerizedWiki/LocalSettings.php
@@ -139,10 +139,40 @@ wfLoadSkin( 'Vector' );
 # wfLoadExtension( 'ExtensionName' );
 # to LocalSettings.php. Check specific extension documentation for more details.
 # The following extensions were automatically enabled:
+wfLoadExtension( 'CategoryTree' );
+wfLoadExtension( 'Cite' );
+wfLoadExtension( 'CiteThisPage' );
+wfLoadExtension( 'CodeEditor' );
+wfLoadExtension( 'ConfirmEdit' );
+wfLoadExtension( 'Gadgets' );
+wfLoadExtension( 'ImageMap' );
+wfLoadExtension( 'InputBox' );
+wfLoadExtension( 'Interwiki' );
+wfLoadExtension( 'LocalisationUpdate' );
+wfLoadExtension( 'MultimediaViewer' );
+wfLoadExtension( 'Nuke' );
 wfLoadExtension( 'OATHAuth' );
+wfLoadExtension( 'PageImages' );
+wfLoadExtension( 'ParserFunctions' );
+wfLoadExtension( 'PdfHandler' );
+wfLoadExtension( 'Poem' );
+wfLoadExtension( 'Renameuser' );
+wfLoadExtension( 'ReplaceText' );
+wfLoadExtension( 'Scribunto' );
+wfLoadExtension( 'SecureLinkFixer' );
+wfLoadExtension( 'SpamBlacklist' );
+wfLoadExtension( 'SyntaxHighlight_GeSHi' );
+wfLoadExtension( 'TemplateData' );
+wfLoadExtension( 'TextExtracts' );
+wfLoadExtension( 'TitleBlacklist' );
+# The following extension requires to instsallation of Parsoid server
+# wfLoadExtension( 'VisualEditor' );
+wfLoadExtension( 'WikiEditor' );
+
 
 
 # End of automatically generated settings.
 # Add more configuration options below.
+# The following extensions are added in the Dockerfile implemented for bkoo/mediawiki:1.35
 
 wfLoadExtension( 'intersection' );
