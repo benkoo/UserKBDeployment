dfe7e00b5e984c0f771ee267e5b435c3286402a3
Ben Koo <koo0905@gmail.com>
2021-03-27 00:09:16 +0800

    Implemented cron triggered backup

---
+++ dockerizedWiki/AdvancedTooling/DockerBuild/mediawiki/extensions/BackupAndRestore/restore.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# This variable should have the same value as the variable $wgResourceBasePath in LocalSettings.php
+ResourceBasePath="/var/www/html"
+
+# Define the file name format and variable name
+FilePrefix="XLP"
+FilePostfix=".xml"
+
+# Define the latest literal string
+LATEST="LATEST"
+
+# Construct the file name string
+FileName=$FilePrefix$LATEST$FilePostfix
+
+echo "Ready to import all textual data from " $ResourceBasePath/images/$FileName
+
+php $ResourceBasePath/maintenance/importDump.php < $ResourceBasePath/images/$FileName
+
+# Load images from the UploadedFiles location.
+cd $ResourceBasePath
+php $ResourceBasePath/maintenance/importImages.php $ResourceBasePath/images/UploadedFiles/
--- dockerizedWiki/up.sh
+++ dockerizedWiki/up.sh
@@ -12,7 +12,7 @@ if [[ $(which docker) && $(docker --version) ]]; then
       cygwin*)  echo "$OSTYPE should install Docker Desktop by following this link https://docs.docker.com/docker-for-windows/install/" ;;
       linux*)
         echo "Some $OSTYPE distributions could install Docker, we will try to install Docker for you..." 
-        ./scripts/installDockerForUbuntu.sh   
+        ./AdvancedTooling/installDockerForUbuntu.sh   
         echo "Installation complete, setting up the sudo su command, you will need the root access to this linux machine."
         sudo su ;;
       *)        echo "Sorry, this $OSTYPE might not have Docker implementation" ;;
@@ -27,30 +27,31 @@ fi
 # See https://stackoverflow.com/questions/2264428/how-to-convert-a-string-to-lower-case-in-bash
 # However, it will not work in Mac OS X, since it is still using Bash v 3.2
 LOWERCASE_CURRENTDIR="$(tr [A-Z] [a-z] <<< "${PWD##*/}")"
-MW_CONTAINER=$LOWERCASE_CURRENTDIR"_mediawiki_1"
+# MW_CONTAINER=$LOWERCASE_CURRENTDIR"_mediawiki_1"
+DB_CONTAINER=$LOWERCASE_CURRENTDIR"_database_1"
 
 # This variable should have the same value as the variable $wgResourceBasePath in LocalSettings.php
-ResourceBasePath="/var/www/html"
+# ResourceBasePath="/var/www/html"
 
-BACKUPSCRIPTFULLPATH=$ResourceBasePath"/extensions/BackupAndRestore/backup.sh"
-RESOTRESCRIPTFULLPATH=$ResourceBasePath"/extensions/BackupAndRestore/restore.sh"
+# BACKUPSCRIPTFULLPATH=$ResourceBasePath"/extensions/BackupAndRestore/backup.sh"
+# RESOTRESCRIPTFULLPATH=$ResourceBasePath"/extensions/BackupAndRestore/restore.sh"
 
-echo "Executing: " docker exec $MW_CONTAINER $BACKUPSCRIPTFULLPATH
-docker exec $MW_CONTAINER $BACKUPSCRIPTFULLPATH
+# echo "Executing: " docker exec $MW_CONTAINER $BACKUPSCRIPTFULLPATH
+# docker exec $MW_CONTAINER $BACKUPSCRIPTFULLPATH
 # stop all docker processes
 docker-compose down --volumes
 
 # If the mountPoint directory doesn't exist, 
 # Decompress the InitialDataPackage to ./mountPoint 
 if [ ! -e ./mountPoint/ ]; then
-tar -xzvf ./InitialDataPackage.tar.gz -C .
+  tar -xzvf ./InitialDataPackage.tar.gz -C .
 fi
 
 # Start the docker processes
 docker-compose up -d
 # After docker processes are ready, reload the data from earlier dump
-echo "Loading data from earlier backups..."
-echo "Executing: " docker exec $MW_CONTAINER $RESOTRESCRIPTFULLPATH
-docker exec $MW_CONTAINER $RESOTRESCRIPTFULLPATH
+# echo "Loading data from earlier backups..."
+# echo "Executing: " docker exec $MW_CONTAINER $RESOTRESCRIPTFULLPATH
+# docker exec $MW_CONTAINER $RESOTRESCRIPTFULLPATH
 
-
+docker exec $DB_CONTAINER service cron start
